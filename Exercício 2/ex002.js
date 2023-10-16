27c758f1232a3fcfb0a9835e6c49a9bae29d2abb
Ricardo Simões <147839494+ricardoszsimoes@users.noreply.github.com>
2023-10-16 09:50:52 -0300

    Atividade JavaScript v1.1
    - Acionado If e Else no exercício 2.
    
    - Adicionado mensagem de erro para quando o usuário tenta dividir usando 0 (zero).

--- Exercício 2/ex002.js
+++ Exercício 2/ex002.js
@@ -1,6 +1,13 @@
 var numero1 = window.prompt('Escolha um número');
 var numero2 = window.prompt('Escolha outro número');
 
-var resultado = numero1 / numero2;
-
-window.alert(resultado); 
+if (numero1==0) {
+    window.alert('Não pode dividir com zero');
+} else {
+    if (numero2 == 0) {
+        window.alert('Não pode dividir com zero');
+    } else {
+        var resultado = numero1 / numero2;
+        window.alert('Esse é o resultado: ' + resultado);
+    }
+}
